f7ee5ef560d3142da0ff0ca77c778882dfdccd93
School <School@Tobys-MacBook-Pro.local>
2020-02-25 11:39:45 -0800

    assignment desc added

--- practiceTOBY.py
+++ practiceTOBY.py
@@ -1,6 +1,7 @@
 # Partner 1: Toby
 # Partner 2: Justin
 ########################
+# Assignment Name: GitHub Practice
 
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
