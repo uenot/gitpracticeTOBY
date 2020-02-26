0024033de4bb0f42bc4dca957bf2c1cba2bce774
School <School@Tobys-MacBook-Pro.local>
2020-02-26 11:18:34 -0800

    fixed missing parentheses

--- practiceTOBY.py
+++ practiceTOBY.py
@@ -4,22 +4,22 @@
 # Assignment Name: GitHub Practice- 20 points- 2/25/20
 
 def getNRandom(n):
-	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
+    '''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
     nums = []
     import random as rand
     for i in range(n):
-        nums.append(rand.randint(1, 10)
+        nums.append(rand.randint(1, 10))
     return nums
 
 def multiplyRandom(numbers):
-	'''takes in a list of n numbers and returns the product of the numbers'''
+    '''takes in a list of n numbers and returns the product of the numbers'''
     product = 1
     for number in numbers:
         product = product*int(number)
     return product
 
 def main():
-	print(multiplyRandom(getNRandom(10))
+    print(multiplyRandom(getNRandom(10)))
 
 if __name__ == "__main__":
-	main()
+    main()
